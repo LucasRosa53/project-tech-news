58596abce4fdac3a678eeeb907919a4fa829343f
LucasRosa53 <lucasrosa223@hotmail.com>
2023-09-12 16:17:34 -0300

    resolvido req4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -37,9 +37,22 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+    new_news = {
+        "url": selector.css("link[rel=canonical]::attr(href)").get(),
+        "title": selector.css('h1.entry-title::text').get().strip(),
+        "timestamp": selector.css('li.meta-date::text').get(),
+        "writer": selector.css('span.author a::text').get(),
+        "reading_time": int(selector.css(
+            '.meta-reading-time::text'
+        ).re_first(r'\d+')),
+        "summary": ''.join(selector.css(
+            ".entry-content > p:first-of-type *::text"
+        ).getall()).strip(),
+        "category": selector.css('.category-style span.label::text').get()
+    }
 
+    return new_news
 
 # Requisito 5
 def get_tech_news(amount):
